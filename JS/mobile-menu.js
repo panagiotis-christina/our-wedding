e13a61d0796cc23e6314edd34efeeb941352d616
panagiotis-christina <panopet_4@hotmail.gr>
2025-07-13 20:18:40 +0300

    Add files via upload

---
+++ JS/mobile-menu.js
@@ -0,0 +1,9 @@
+  function openNav() {
+    document.getElementById("mySidebar").style.width = "300px";
+    document.getElementById("mob-main").style.marginLeft = "0";
+  }
+
+  function closeNav() {
+    document.getElementById("mySidebar").style.width = "0";
+    document.getElementById("mob-main").style.marginLeft= "0";
+  }
