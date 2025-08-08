ef48ce060d8df35dfa5cd3e2c09a5c00ee6c5bf1
panagiotis-christina <panopet_4@hotmail.gr>
2025-08-08 18:25:23 +0300

    Add files via upload

--- javascript.js
+++ javascript.js
@@ -23,6 +23,139 @@ $(document).ready(function() {
 	});
 });
 
+
+// Set the date we're counting down to
+var countDownDate = new Date("Dec 28, 2025 17:00:00").getTime();
+
+// Update the count down every 1 second
+var x = setInterval(function() {
+
+  // Get today's date and time
+  var now = new Date().getTime();
+
+  // Find the distance between now and the count down date
+  var distance = countDownDate - now;
+
+  // Time calculations for days, hours, minutes and seconds
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  // Output the result in an element with id="countdown"
+  document.getElementById("countdown").innerHTML = "<span class='date-block'><span class='num'>" +days + "</span><span class='text'>Μέρες</span></span><span class='date-block'><span class='num'>" + hours + "</span><span class='text'>Ώρες</span></span><span class='date-block'><span class='num'>"
+  + minutes + "</span><span class='text'>Λεπτά</span></span><span class='date-block'><span class='num'>" + seconds + "</span><span class='text'>Δευτ.</span></span>";
+
+  // If the count down is over, write some text
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("countdown").innerHTML = "";
+	document.getElementById("fireworks").style.display = "block";
+  }
+}, 1000);
+
+const canvas = document.getElementById('fireworks');
+const ctx = canvas.getContext('2d');
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+class Particle {
+    constructor(x, y, color) {
+        this.x = x;
+        this.y = y;
+        this.color = color;
+        this.velocity = {
+            x: (Math.random() - 0.5) * 8,
+            y: (Math.random() - 0.5) * 8
+        };
+        this.alpha = 1;
+        this.friction = 0.99;
+    }
+
+    draw() {
+        ctx.globalAlpha = this.alpha;
+        ctx.beginPath();
+        ctx.arc(this.x, this.y, 2, 0, Math.PI * 2, false);
+        ctx.fillStyle = this.color;
+        ctx.fill();
+    }
+
+    update() {
+        this.velocity.x *= this.friction;
+        this.velocity.y *= this.friction;
+        this.x += this.velocity.x;
+        this.y += this.velocity.y;
+        this.alpha -= 0.01;
+    }
+}
+
+class Firework {
+    constructor(x, y, color) {
+        this.x = x;
+        this.y = y;
+        this.color = color;
+        this.velocity = {x: 0, y: Math.random() * -2.5 - 0.5};
+        this.particles = [];
+        this.lifespan = 180;
+        this.hasExploded = false;
+    }
+
+    draw() {
+        ctx.beginPath();
+        ctx.arc(this.x, this.y, 3, 0, Math.PI * 2, false);
+        ctx.fillStyle = this.color;
+        ctx.fill();
+    }
+
+    explode() {
+        for (let i = 0; i < 50; i++) {
+            this.particles.push(new Particle(this.x, this.y, this.color));
+        }
+    }
+
+    update() {
+        this.lifespan--;
+
+        if (this.lifespan <= 0 && !this.hasExploded) {
+            this.explode();
+            this.velocity = {x: 0, y: 0};
+            this.hasExploded = true;
+        } else if (this.lifespan > 0) {
+            this.y += this.velocity.y;
+        }
+
+        for (let i = 0; i < this.particles.length; i++) {
+            this.particles[i].update();
+            this.particles[i].draw();
+        }
+    }
+}
+
+let fireworks = [];
+
+   function animate() {
+	   requestAnimationFrame(animate);
+	   ctx.fillStyle = 'rgba(255, 255, 255, 0.1)';
+	   ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+	   fireworks.forEach((firework, index) => {
+		   firework.update();
+		   firework.draw();
+
+		   if (firework.lifespan <= 0 && firework.particles.every(p => p.alpha <= 0)) {
+			   fireworks.splice(index, 1);
+		   }
+	   });
+
+	   if (Math.random() < 0.015) {
+	   const x = Math.random() * canvas.width;
+	   const color = `hsl(${Math.random() * 360}, 50%, 50%)`;
+	   fireworks.push(new Firework(x, canvas.height, color));
+   }
+}
+
+animate();
+
 function openInfo(evt, infoName) {
   var i, tabcontent, tablinks;
   tabcontent = document.getElementsByClassName("tabcontent");
